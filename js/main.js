aacbf7c03b12978a7e2e4a4f66f3c6f6bcd8ef93
Luke Sweeney <luke@thesweeneys.org>
2017-10-31 19:40:54 -0500

    Adds carousel and makes it usable with the tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$('.carousel.carousel-slider').carousel({ fullWidth: true });
+
 var app = new Vue({
   el: "#main",
   data: {
@@ -7,7 +9,14 @@ var app = new Vue({
   methods: {
     setActiveTab(tabName) {
       this.activeTab = tabName;
+      this.updateImage(tabName);
       return true;
-    }
+    },
+    updateImage(selectedTab) {
+      // Get the floor number and subtract 3 to make up for the index offset
+      var index = selectedTab.replace(/^\D+/g, '') - 3;
+      $('.carousel').carousel('set', index);
+    },
+
   }
 });
